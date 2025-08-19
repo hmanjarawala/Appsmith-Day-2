2e12ca8195899d2b7e7e822d35c207f25283f8e3
himanshu.manjarawala <himanshu.manjarawala@automationanywhere.com>
2025-08-19 15:02:17 +0000

    async function added

--- pages/Users From the Country/jsobjects/Utils/Utils.js
+++ pages/Users From the Country/jsobjects/Utils/Utils.js
@@ -6,5 +6,16 @@ export default {
 			label: g.gender,
 			value: g.gender
 		}))
+	},
+	async selectUsersFromCountry(){
+		try{
+			const contries = await FindCountry.run();
+			const userFromCountry = await FindUserFromCountry.run({
+				country:  contries[0].country
+			});
+			return userFromCountry;
+		}catch(e){
+			console.log(e);
+		}
 	}
 }
